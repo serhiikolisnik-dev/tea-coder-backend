f7bac349a489ee39d73c4c50496a31cfd9de4265
serhiikolisnik-dev <serhii.kolisnik@gmail.com>
2025-10-09 23:04:26 +0300

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'root',
+      password: '123456',
+      database: 'nestjs-course',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
