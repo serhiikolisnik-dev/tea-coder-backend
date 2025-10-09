c228e0ee6ba982393e5de7bf60fc622da90c534d
serhiikolisnik-dev <serhii.kolisnik@gmail.com>
2025-10-09 18:58:28 +0300

    decorator Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
 
 @Module({
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { MovieService } from './movie.service';
 
-@Controller('movie')
+@Controller({
+  path: 'movie',
+  host: ['api.themovie.com', 'api.googleapis.com'],
+})
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 }
