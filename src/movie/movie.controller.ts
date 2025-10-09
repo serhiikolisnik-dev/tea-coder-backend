adc5e56cca7e05fac7de48830f7159e2cfad9555
serhiikolisnik-dev <serhii.kolisnik@gmail.com>
2025-10-09 20:23:29 +0300

    decorator of params

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,10 +1,75 @@
-import { Controller } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Body,
+  Headers,
+  Req,
+  Res,
+  Param,
+  Session,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
 
-@Controller({
-  path: 'movie',
-  host: ['api.themovie.com', 'api.googleapis.com'],
-})
+@Controller('movies')
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+  private movies = [
+    {
+      title: 'Sport',
+    },
+    {
+      title: 'Movies',
+    },
+  ];
+
+  @Get()
+  getMovies(@Query() query: any) {
+    return JSON.stringify(query);
+  }
+
+  @Get('by-id/:id')
+  getMovieBuID(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  getBody(@Body() body: { title: string; desc: string }) {
+    return body;
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return headers;
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return { userAgent };
+  }
+
+  @Get('request')
+  getRequestDetails(@Req() req: Request) {
+    return {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      query: req.query,
+      params: req.params,
+    };
+  }
+
+  @Get('response')
+  getResponseDetails(@Res() res: Response) {
+    res
+      .status(201)
+      .json({ message: 'Successfully retrieved response details' });
+  }
+
+  @Get('session')
+  getSessionDetails(@Session() session: Record<string, any>) {
+    return session;
+  }
 }
